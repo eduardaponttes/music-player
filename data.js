5b1b4aec0d9aa2c67b79445e53d5b31ee4bb39cf
eduardaponttes <121953125+eduardaponttes@users.noreply.github.com>
2023-01-25 11:41:47 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+let songs = [
+    {
+        name: 'Bags',
+        path: "Song 1.mp3",
+        artist: 'Clairo',
+        cover: 'cover-1.png'
+    },
+    {
+        name: 'EARFQUAKE',
+        path: 'Song 2.mp3',
+        artist: 'Tyler The Creator',
+        cover: 'cover-2.png'
+    },
+    {
+        name: 'Redbone',
+        path: 'Song 3.mp3',
+        artist: 'Childish Gambino',
+        cover: 'cover-3.png'
+    },
+    {
+        name: 'You Right',
+        path: 'Song 4.mp3',
+        artist: 'Doja Cat',
+        cover: 'cover-4.png'
+    },
+    {
+        name: '2F2',
+        path: 'Song 5.mp3',
+        artist: 'SZA',
+        cover: 'cover-5.png'
+    },
+    {
+        name: 'cardigan',
+        path: 'Song 6.mp3',
+        artist: 'Taylor Swift',
+        cover: 'cover-6.png'
+    },
+]
